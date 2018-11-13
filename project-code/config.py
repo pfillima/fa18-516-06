bdb11d2c801ad432333b287e53cbf5804aaf22b4
Paul Filliman <pfillima@iu.edu>
2018-11-13 14:22:49 -0500

    Create config.py

---
+++ project-code/config.py
@@ -0,0 +1,7 @@
+CONN_STRING = 'DRIVER={FreeTDS};
+                SERVER=pfilliman.database.windows.net;
+                PORT=1433;
+                DATABASE=AdvWorksLT
+                UID=sqladmin;
+                PWD=SqlAdmin!@123;
+                TDS_Version=7.0'  
