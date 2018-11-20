a1e42cbe2c07552eb1acc5a44c7cd778356c1ebd
Paul Filliman <pfillima@iu.edu>
2018-11-19 20:22:40 -0500

    Update config.py

--- project-code/config.py
+++ project-code/config.py
@@ -1,2 +1,2 @@
-CONN_STRING = 'DRIVER={FreeTDS};SERVER=pfillimansql.database.windows.net;PORT=1433;DATABASE=AdvWorksLT;UID=sqladmin;PWD=SqlDba123;TDS_Version=7.0' 
+CONN_STRING = 'DRIVER={FreeTDS};SERVER=pfillimansql.database.windows.net;PORT=1433;DATABASE=AdvWorksLT;UID=sqladmin;PWD=XXXXXXXXX;TDS_Version=7.0' 
  
